14b7c3f530c67b23fc409e8392829bb18e7424c1
Chirashree Pan <chirashreepan42@gmail.com>
2026-04-26 21:22:04 +0530

    changes new

--- one.py
+++ one.py
@@ -1,10 +1,4 @@
-def selection(arr):
-    n=len(arr)
-    for i in range(n-1):
-        for j in range(i+1,n):
-            if arr[i]>arr[j]:
-                arr[i],arr[j]=arr[j],arr[i]
-    return arr
-arr=[20,9,8,7]
-print(selection(arr))            
+def show(n):
+    print(n)
+show(3)              
         
