8ed8c92b545d21b4b9ffa48f3843621f81062fa4
CHIRASHREE PAN <chirashreepan42@gmail.com>
2026-04-26 21:29:32 +0530

    Refactor i function and add print statement
    Removed insertion sort implementation and added print statement for length.

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 def i(arr):
     n=len(arr)
-    for i in range(1,n):
-        n=len
-        curr=arr[i]
-        j=i-1
-        while j>=0 and arr[j]>curr:
-            arr[j+1]=arr[j]
-            j=j-1
-        arr[j+1]=curr
-    return arr
+    print(n)
 arr=[12,11,13,5,6]
 print(i(arr))            
         
